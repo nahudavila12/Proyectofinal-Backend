04599b3ebc6b347735fc26f164b7c40d26a37d5e
kikecastillocolombia <enriquecastiyosoy@gmail.com>
2024-09-18 17:21:56 -0500

    cloudinary con streamifier

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CloudinaryConfig } from './config/cloudinary';
+import { CloudinaryService } from './commons/cloudinary.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CloudinaryConfig, CloudinaryService],
 })
 export class AppModule {}
