43c2ce99fdcaaa802f690585d12682c3c1332563
dante <danterossi99@gmail.com>
2024-09-20 16:11:04 -0300

    documentacion en entities

--- Back/src/rooms/room.entity.ts
+++ Back/src/rooms/room.entity.ts
@@ -1,58 +1,91 @@
-import{
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    OneToMany,
-    ManyToOne,
-}from 'typeorm';
-import { Reservation } from '../reservations/reservation.entity'
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToOne,
+} from 'typeorm';
+import { Reservation } from '../reservations/reservation.entity';
 import { Property } from '../propierties/property.entity';
 import { RoomImg } from './roomImg.entity';
 import { RoomService } from './roomService.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export enum ICategories {
-    STANDARD = 'standard',
-    DELUXE = 'deluxe',
-    SUITE = 'suite',
+  STANDARD = 'standard',
+  DELUXE = 'deluxe',
+  SUITE = 'suite',
 }
 
-export enum IRoomState{
-    Avaiable = 'avaiable',
-    Reserved = 'reserved',
-    Occupied = 'occupied',
-    Maintenance = 'maintenance'
+export enum IRoomState {
+  Avaiable = 'avaiable',
+  Reserved = 'reserved',
+  Occupied = 'occupied',
+  Maintenance = 'maintenance',
 }
 
 @Entity('rooms')
-export class Room{
+export class Room {
+  @ApiProperty({
+    example: 'e5bdda8e-3da4-4a5f-b9ea-6239c73222f4',
+    description: 'UUID de la habitación',
+  })
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    uuid: string;
+  @ApiProperty({ example: 101, description: 'Número de la habitación' })
+  @Column()
+  room_number: number;
 
-    @Column()
-    room_number: number; 
+  @ApiProperty({
+    enum: ICategories,
+    example: ICategories.STANDARD,
+    description: 'Categoría de la habitación',
+  })
+  @Column({ type: 'enum', enum: ICategories })
+  category: ICategories;
 
-    @Column({type: 'enum', enum: ICategories })
-    category: ICategories;
+  @ApiProperty({ example: 2, description: 'Capacidad de la habitación' })
+  @Column()
+  capacity: number;
 
-    @Column()
-    capacity: number;
+  @ApiProperty({ example: 150.0, description: 'Precio por día en USD' })
+  @Column()
+  price_per_day: number;
 
-    @Column()
-    price_per_day: number
+  @ApiProperty({
+    enum: IRoomState,
+    default: IRoomState.Avaiable,
+    description: 'Estado de disponibilidad de la habitación',
+  })
+  @Column({ type: 'enum', enum: IRoomState, default: IRoomState.Avaiable })
+  disponibility: IRoomState;
 
-    @Column({ type: 'enum', enum: IRoomState, default: IRoomState.Avaiable })
-    disponibility: IRoomState
+  @ApiProperty({
+    type: () => [RoomImg],
+    description: 'Imágenes de la habitación',
+  })
+  @OneToMany(() => RoomImg, (roomImg) => roomImg.room)
+  img: RoomImg[];
 
-    @OneToMany(() => RoomImg, (roomImg) => roomImg.room)
-    img: RoomImg[];
+  @ApiProperty({
+    type: () => [Reservation],
+    description: 'Reservaciones de la habitación',
+  })
+  @OneToMany(() => Reservation, (reservation) => reservation.room)
+  reservation: Reservation[];
 
-    @OneToMany(() => Reservation, (reservation) => reservation.room)
-    reservation: Reservation[]
-    
-    @OneToMany(() => RoomService, (roomService) => roomService.room)
-    services: RoomService[];
+  @ApiProperty({
+    type: () => [RoomService],
+    description: 'Servicios de la habitación',
+  })
+  @OneToMany(() => RoomService, (roomService) => roomService.room)
+  services: RoomService[];
 
-    @ManyToOne(() => Property, (property) => property.room)
-    property: Property
+  @ApiProperty({
+    type: () => Property,
+    description: 'Propiedad a la que pertenece la habitación',
+  })
+  @ManyToOne(() => Property, (property) => property.room)
+  property: Property;
 }
