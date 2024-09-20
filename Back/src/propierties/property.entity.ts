1dec7a65b7a734ad8fa4c31b97b8c25f188b95c4
nahudavila12 <nahu.davila@gmail.com>
2024-09-20 12:12:48 -0300

    ARReglo 3

--- Back/src/propierties/property.entity.ts
+++ Back/src/propierties/property.entity.ts
@@ -1,50 +1,50 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    ManyToOne,
-    Column,
-    JoinColumn,
-    OneToMany,
-  } from 'typeorm';
-  
-  import { Owner } from '../owners/owner.entity';
-  import { Room } from '../rooms/room.entity';
-  import { PropertyImg } from './propertyImg.entity';
-  
-  export enum PropertyType {
-    HOTEL = 'hotel',
-    CABANA = 'cabaña',
-    DEPARTAMENTO = 'departamento',
-  }
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  Column,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
+
+import { Owner } from '../owners/owner.entity';
+import { Room } from '../rooms/room.entity';
+import { PropertyImg } from './propertyImg.entity';
+
+export enum PropertyType {
+  HOTEL = 'hotel',
+  CABANA = 'cabaña',
+  DEPARTAMENTO = 'departamento',
+}
 
 
-  @Entity('Properties')
+@Entity('Properties')
 export class Property {
-    @PrimaryGeneratedColumn('uuid')
-    uuid: string;
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
 
-    @Column({ nullable: false })
-    name: string;
+  @Column({ nullable: false })
+  name: string;
 
-    @Column()
-    location: string;
+  @Column()
+  location: string;
 
-    @Column({
-      type: 'enum',
-      enum: PropertyType,
-    })
-    propertyType: PropertyType;
+  @Column({
+    type: 'enum',
+    enum: PropertyType,
+  })
+  propertyType: PropertyType;
 
-    @Column({ default: false }) 
-    isBanned: boolean; 
+  @Column({ default: false }) 
+  isBanned: boolean; 
 
-    @OneToMany(() => PropertyImg, (propertyImg) => propertyImg.property)
-    img: PropertyImg[];
+  @OneToMany(() => PropertyImg, (propertyImg) => propertyImg.property)
+  img: PropertyImg[];
 
-    @ManyToOne(() => Owner, (owner) => owner.property)
-    @JoinColumn()
-    owner: Owner;
+  @ManyToOne(() => Owner, (owner) => owner.property)
+  @JoinColumn()
+  owner: Owner;
 
-    @OneToMany(() => Room, (room) => room.property)
-    room: Room[];
+  @OneToMany(() => Room, (room) => room.property)
+  room: Room[];
 }
