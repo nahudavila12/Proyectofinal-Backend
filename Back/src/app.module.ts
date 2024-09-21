44fa1837e6460d39f6247c0f3a82b108f26a6d83
dante <danterossi99@gmail.com>
2024-09-21 02:10:04 -0300

    DB seeding. No pude probarlo porque no levanta el server todavia

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -4,25 +4,26 @@ import { AppService } from './app.service';
 
 import { CloudinaryConfig } from './config/cloudinary';
 import { CloudinaryService } from './commons/cloudinary.service';
-=======
+import { SeedCommand } from './seeds/seed.command';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import typeOrmConfig from './config/typeorm.config';
-
+import typeOrmConfig from './Config/typeOrm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
-    load: [typeOrmConfig]
-  }),
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
     TypeOrmModule.forRootAsync({
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService) => configService.get('typeOrm') 
-})],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeOrm'),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, CloudinaryConfig, CloudinaryService],
+  providers: [AppService, CloudinaryConfig, CloudinaryService, SeedCommand],
 })
-
 export class AppModule {}
--- Back/src/propierties/property.entity.ts
+++ Back/src/propierties/property.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, JoinColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  Column,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
 import { Owner } from '../owners/owner.entity';
 import { Room } from '../rooms/room.entity';
 import { PropertyImg } from './propertyImg.entity';
@@ -12,33 +19,56 @@ export enum PropertyType {
 
 @Entity('Properties')
 export class Property {
-
-  @ApiProperty({ example: 'e5bdda8e-3da4-4a5f-b9ea-6239c73222f4', description: 'UUID de la propiedad' })
+  @ApiProperty({
+    example: 'e5bdda8e-3da4-4a5f-b9ea-6239c73222f4',
+    description: 'UUID de la propiedad',
+  })
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
-  @ApiProperty({ example: 'Hotel California', description: 'Nombre de la propiedad' })
+  @ApiProperty({
+    example: 'Hotel California',
+    description: 'Nombre de la propiedad',
+  })
   @Column({ nullable: false })
   name: string;
 
-  @ApiProperty({ example: '123 Calle Principal, Ciudad', description: 'Ubicación de la propiedad' })
+  @ApiProperty({
+    example: '123 Calle Principal, Ciudad',
+    description: 'Ubicación de la propiedad',
+  })
   @Column()
   location: string;
 
-  @ApiProperty({ enum: PropertyType, example: PropertyType.HOTEL, description: 'Tipo de propiedad' })
+  @ApiProperty({
+    enum: PropertyType,
+    example: PropertyType.HOTEL,
+    description: 'Tipo de propiedad',
+  })
   @Column({
     type: 'enum',
     enum: PropertyType,
   })
   propertyType: PropertyType;
 
-  @ApiProperty({ example: false, description: 'Si la propiedad está prohibida o no' })
+  @ApiProperty({
+    example: false,
+    description: 'Si la propiedad está prohibida o no',
+  })
   @Column({ default: false })
   isBanned: boolean;
 
-  @ApiProperty({ type: () => [PropertyImg], description: 'Imágenes de la propiedad' })
+  @ApiProperty({
+    type: () => [PropertyImg],
+    description: 'Imágenes de la propiedad',
+  })
   @OneToMany(() => PropertyImg, (propertyImg) => propertyImg.property)
   img: PropertyImg[];
 
-  @ApiProperty({ type: () => Owner, description: 'Propietario de la propiedad' })
+  @ApiProperty({
+    type: () => Owner,
+    description: 'Propietario de la propiedad',
+  })
   @ManyToOne(() => Owner, (owner) => owner.property)
+  owner: Owner;
+}
