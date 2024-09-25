3a7a54fdaa1afb75a6ebf81a0f2612d47463ddfb
dante <danterossi99@gmail.com>
2024-09-25 11:47:08 -0300

    reservation y sistema de guardian con IROL

---
+++ Back/src/guards/roles.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from './roles.decorator';
+import { IRol } from 'src/users/user.entity'; // Importamos el enum para hacer comparaciones
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<IRol[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    return requiredRoles.some((role) => user.roles?.includes(role));
+  }
+}
