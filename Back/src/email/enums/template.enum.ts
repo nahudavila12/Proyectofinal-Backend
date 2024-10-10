145eeb1a5456a54564e5a5296a317f3cc77b47d9
Enrique Castillo <168988661+kikecastillocolombia@users.noreply.github.com>
2024-10-10 16:05:50 -0500

    Correo ban (#69)
    * email confirmacion reserva
    
    * email pago paypal
    
    * correo actualizacion reserva
    
    * correo usuario baneado
    
    ---------
    
    Co-authored-by: Nahuel Davila | Full Stack Developer <160268045+nahudavila12@users.noreply.github.com>

--- Back/src/email/enums/template.enum.ts
+++ Back/src/email/enums/template.enum.ts
@@ -2,7 +2,8 @@ export enum Template {
     WELCOME = 'welcome',
     CONFIRMATION = 'confirmation',
     PAYPAL_PAYMENT = 'paypal_payment',
-    UPDATE_RESERVATION = 'update_reservation'
 
+    UPDATE_RESERVATION = 'update_reservation',
+    BAN_NOTIFICATION = "ban_notification"
 
 }
