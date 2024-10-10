ce340894308be39f05854808c6f367911dc51b70
Enrique Castillo <168988661+kikecastillocolombia@users.noreply.github.com>
2024-10-10 16:07:34 -0500

    Correo nuevo propietario (#70)
    * email confirmacion reserva
    
    * email pago paypal
    
    * correo actualizacion reserva
    
    * correo usuario baneado
    
    * correo Nuevo Propietario
    
    ---------
    
    Co-authored-by: Nahuel Davila | Full Stack Developer <160268045+nahudavila12@users.noreply.github.com>

--- Back/src/email/enums/template.enum.ts
+++ Back/src/email/enums/template.enum.ts
@@ -2,8 +2,8 @@ export enum Template {
     WELCOME = 'welcome',
     CONFIRMATION = 'confirmation',
     PAYPAL_PAYMENT = 'paypal_payment',
-
     UPDATE_RESERVATION = 'update_reservation',
-    BAN_NOTIFICATION = "ban_notification"
+    BAN_NOTIFICATION = "ban_notification",
+    OWNER_CONFIRMATION = "owner_confirmation"
 
 }
